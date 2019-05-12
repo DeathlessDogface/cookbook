782b858b7aa14bc82ac7d1287228c1822543b581
jiaosj2 <jiaosj2@lenovo.com>
2019-05-12 17:22:56 +0800

    fix

--- get_diff_file_from_dir.py
+++ get_diff_file_from_dir.py
@@ -3,14 +3,15 @@
 import hashlib
 import os
 import sys
-
+import pdb
+#pdb.set_trace()
 
 def get_diff(src_dir, other):
     diff_file_list = []
     for cur_dir, sub_dirs, sub_files in os.walk(src_dir):
         for cur_file in sub_files:
             src_file_path = os.path.join(cur_dir, cur_file)
-            other_file_path = os.path.join(other, src_file_path.split(os.path.altsep, 1)[1])
+            other_file_path = os.path.join(other, src_file_path.split(os.path.sep, 1)[1])
             if os.path.isfile(other_file_path):
                 src_md5 = hashlib.md5()
                 with open(src_file_path, 'r') as src_f:
@@ -19,12 +20,14 @@ def get_diff(src_dir, other):
                 src_md5 = src_md5.hexdigest()
 
                 other_md5 = hashlib.md5()
-                with open(src_file_path, 'r') as src_f:
-                    for src_line in src_f.readlines():
-                        other_md5.update(src_line)
+                with open(other_file_path, 'r') as other_f:
+                    for other_line in other_f.readlines():
+                        other_md5.update(other_line)
                 other_md5 = other_md5.hexdigest()
                 if src_md5 != other_md5:
                     diff_file_list.append(src_file_path)
+                else:
+                    print "same file:",src_file_path
             else:
                 diff_file_list.append(src_file_path)
     return diff_file_list
@@ -36,7 +39,7 @@ def main():
         args = args[1:]
     if "get_diff_file_from_dir" in args[0]:
         args = args[1:]
-    get_diff(args[0], args[1])
+    print get_diff(args[0], args[1])
 
 
 if __name__ == '__main__':
