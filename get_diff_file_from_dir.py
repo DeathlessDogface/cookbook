e427f5cdf5997c0c64c9c7ec69794b91bd2fe9f5
jiaosj2 <jiaosj2@lenovo.com>
2019-05-13 10:19:44 +0800

    fix2

--- get_diff_file_from_dir.py
+++ get_diff_file_from_dir.py
@@ -3,15 +3,15 @@
 import hashlib
 import os
 import sys
-import pdb
-#pdb.set_trace()
 
-def get_diff(src_dir, other):
+
+def get_diff(src_dir, other, arm):
     diff_file_list = []
     for cur_dir, sub_dirs, sub_files in os.walk(src_dir):
         for cur_file in sub_files:
             src_file_path = os.path.join(cur_dir, cur_file)
             other_file_path = os.path.join(other, src_file_path.split(os.path.sep, 1)[1])
+            diff = False
             if os.path.isfile(other_file_path):
                 src_md5 = hashlib.md5()
                 with open(src_file_path, 'r') as src_f:
@@ -24,11 +24,19 @@ def get_diff(src_dir, other):
                     for other_line in other_f.readlines():
                         other_md5.update(other_line)
                 other_md5 = other_md5.hexdigest()
+
                 if src_md5 != other_md5:
-                    diff_file_list.append(src_file_path)
+                    diff = True
                 else:
-                    print "same file:",src_file_path
+                    print "same file:{}".format(src_file_path)
             else:
+                diff = True
+            if diff:
+                arm_file_path = os.path.join(arm, src_file_path.split(os.path.sep, 1)[1])
+                arm_file_dir = os.path.split(arm_file_path)[0]
+                if not os.path.isdir(arm_file_dir):
+                    os.popen("mkdir -p {}".format(arm_file_dir))
+                os.popen("cp -f {} {}".format(src_file_path,arm_file_path))
                 diff_file_list.append(src_file_path)
     return diff_file_list
 
@@ -39,8 +47,9 @@ def main():
         args = args[1:]
     if "get_diff_file_from_dir" in args[0]:
         args = args[1:]
-    print get_diff(args[0], args[1])
+    print get_diff(args[0], args[1], args[2])
 
 
 if __name__ == '__main__':
     main()
+
